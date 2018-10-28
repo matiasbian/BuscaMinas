0aceeff243ba3658473e9ff878f3346097a1eb35
matiasbian <matiasezequielbian@gmail.com>
2018-10-27 21:29:07 -0300

    nearly minas system

--- Button.gd
+++ Button.gd
@@ -1,5 +1,7 @@
 extends Button
 var soyCoco = false
+var indice = 0
+var minasCerca = 0
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -14,11 +16,42 @@ func _ready():
 
 func clickAction():
 	if (soyCoco):
-		print("Coco")
-		self.text = "X"
+		print("Coco" + str(indice.x) + "," + str(indice.y))
+		marcoMinas()
 	else:
-		print("todoBien")
-		self.text = "OK"
+		if (Manag.PrimerMov):
+			Manag.colocoMinas(indice)
+		print("todoBien" + str(indice.x) + "," + str(indice.y))
+		verificoMinasCercanas()
+		self.text = str(minasCerca) 
+		
+func verificoMinasCercanas():
+	verificoEnPosicion(Vector2(indice.x,indice.y-1) ) #arriba
+	verificoEnPosicion(Vector2(indice.x,indice.y+1) ) #Abajo
+	verificoEnPosicion(Vector2(indice.x-1,indice.y) ) #Izquierda
+	verificoEnPosicion(Vector2(indice.x+1,indice.y) ) #Derecha
+	verificoEnPosicion(Vector2(indice.x-1,indice.y-1) ) #DiagIzqArriba
+	verificoEnPosicion(Vector2(indice.x-1,indice.y+1) ) #DiagIzqAbajo
+	verificoEnPosicion(Vector2(indice.x+1,indice.y-1) ) #DiagDerArrriba
+	verificoEnPosicion(Vector2(indice.x+1,indice.y+1) ) #DiagDerAbajo
+
+	
+func verificoEnPosicion(nroCelda):
+	if ((nroCelda.x < 0 or nroCelda.x >= Manag.filas ) or (nroCelda.y < 0 or nroCelda.y >= Manag.columnas)):
+		pass
+	else:
+		if (buscoEnLista(nroCelda).soyCoco):
+			minasCerca += 1
+
+func buscoEnLista(indice):
+	for i in range (Manag.listaDeListaFilas.size()):
+		if (Manag.listaDeListaFilas[i].get_node("Button").indice == indice):
+			return Manag.listaDeListaFilas[i].get_node("Button")
+func marcoMinas():
+	for i in range (Manag.listaDeListaFilas.size()):
+		if (Manag.listaDeListaFilas[i].get_node("Button").soyCoco):
+			Manag.listaDeListaFilas[i].get_node("Button").text = "X"
+
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
--- Scripts/Manag.gd
+++ Scripts/Manag.gd
@@ -3,6 +3,39 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-export var listaDeListaFilas = []
+var listaDeListaFilas = []
+var PrimerMov = true
 
+export var filas = 5
+export var columnas = 5
+export var minas = 8
+onready var celda = preload("res://Prefabs/Celda.tscn")
+onready var container = get_node("/root/Node2D/CenterContainer/GridContainer")
+func _ready():
+	randomize()
+	container.columns = columnas
+	for i in range (filas):
+		for j in range(columnas):
+			var celdis  = celda.instance()
+			celdis.get_node("Button").indice = Vector2(i,j)
+			container.add_child(celdis)
+			listaDeListaFilas.append(celdis)
 
+		
+		
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func colocoMinas(celdaTranka):
+	while (minas > 0):
+		var celdis = listaDeListaFilas[rand_range(0,listaDeListaFilas.size() -1)].get_node("Button")
+		if (!celdis.soyCoco and celdis.indice != celdaTranka):
+			minas = minas -1
+			celdis.soyCoco = true
+			PrimerMov = false
+		
